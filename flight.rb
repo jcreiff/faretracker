8170d4a884d18b389c200ab65c4bce00a4f4fdb1
Joe Reiff <jcreiff@gmail.com>
2016-01-19 18:18:23 -0500

    update methods to work with multiple trip options

--- flight.rb
+++ flight.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'time'
+require 'byebug'
 
 class Flight
 
@@ -12,32 +13,33 @@ class Flight
 
 
   def get_data
-    HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=#{ENV["GOOGLE_KEY"]}",
-    :body =>  {
-        "request": {
-          "passengers": {
-            "kind": "qpxexpress#passengerCounts",
-            "adultCount": 1,
-            "childCount": 0,
-            "infantInLapCount": 0,
-            "infantInSeatCount": 0,
-            "seniorCount": 0
-          },
-          "slice": [
-            {
-              "kind": "qpxexpress#sliceInput",
-              "origin": "#{@start_code}",
-              "destination": "#{@end_code}",
-              "date": "#{@date}",
-              "maxStops": 1,
-              "preferredCabin": "COACH",
-            }
-          ],
-          "refundable": false,
-          "solutions": 10
-        }
-      }.to_json,
-      :headers => { 'Content-Type' => 'application/json' })
+    # HTTParty.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=#{ENV["GOOGLE_KEY"]}",
+    # :body =>  {
+    #     "request": {
+    #       "passengers": {
+    #         "kind": "qpxexpress#passengerCounts",
+    #         "adultCount": 1,
+    #         "childCount": 0,
+    #         "infantInLapCount": 0,
+    #         "infantInSeatCount": 0,
+    #         "seniorCount": 0
+    #       },
+    #       "slice": [
+    #         {
+    #           "kind": "qpxexpress#sliceInput",
+    #           "origin": "#{@start_code}",
+    #           "destination": "#{@end_code}",
+    #           "date": "#{@date}",
+    #           "maxStops": 1,
+    #           "preferredCabin": "COACH",
+    #         }
+    #       ],
+    #       "refundable": false,
+    #       "solutions": 10
+    #     }
+    #   }.to_json,
+    #   :headers => { 'Content-Type' => 'application/json' })
+    JSON.parse(File.open('./mock_flight.json').read)
   end
 
   def price(num)
@@ -46,34 +48,34 @@ class Flight
     "$#{new_cost}"
   end
 
-  def carrier(num)
-    @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["flight"]["carrier"]
+  def carrier(trip_num, leg_num)
+    @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["flight"]["carrier"]
   end
 
-  def number(num)
-    @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["flight"]["number"]
+  def number(trip_num, leg_num)
+    @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["flight"]["number"]
   end
 
-  def origin(num)
-    @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["leg"][0]["origin"]
+  def origin(trip_num, leg_num)
+    @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["leg"][0]["origin"]
   end
 
-  def destination(num)
-    @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["leg"][0]["destination"]
+  def destination(trip_num, leg_num)
+    @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["leg"][0]["destination"]
   end
 
-  def departs_on(num)
-    day = @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["leg"][0]["departureTime"]
+  def departs_on(trip_num, leg_num)
+    day = @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["leg"][0]["departureTime"]
     day[0..9]
   end
 
-  def departs_at(num)
-    time = @page["trips"]["tripOption"][0]["slice"][0]["segment"][num]["leg"][0]["departureTime"]
+  def departs_at(trip_num, leg_num)
+    time = @page["trips"]["tripOption"][trip_num]["slice"][0]["segment"][leg_num]["leg"][0]["departureTime"]
     Time.parse(time).strftime '%I:%M %p'
   end
 
-  def total_time(num)
-    minutes = @page["trips"]["tripOption"][num]["slice"][0]["duration"].to_f
+  def total_time(trip_num)
+    minutes = @page["trips"]["tripOption"][trip_num]["slice"][0]["duration"].to_f
     hours = (minutes/60.0).round(2)
     "#{hours} hours"
   end
@@ -85,13 +87,14 @@ class Flight
     until @page["trips"]["tripOption"][trip_counter]["slice"][0]["segment"][leg_counter] == nil
       next_stop = {}
       next_stop["leg"] = leg_counter+1
-      next_stop["origin"] = origin(leg_counter)
-      next_stop["destination"] = destination(leg_counter)
-      next_stop["flight_number"] = "#{carrier(leg_counter)} #{number(leg_counter)}"
-      next_stop["departure_time"] = "#{departs_on(leg_counter)} at #{departs_at(leg_counter)}"
+      next_stop["origin"] = origin(trip_counter, leg_counter)
+      next_stop["destination"] = destination(trip_counter, leg_counter)
+      next_stop["flight_number"] = "#{carrier(trip_counter, leg_counter)} #{number(trip_counter, leg_counter)}"
+      next_stop["departure_time"] = "#{departs_on(trip_counter, leg_counter)} at #{departs_at(trip_counter, leg_counter)}"
       stops << next_stop
       leg_counter += 1
     end
+    stops << price(trip_counter)
     stops
   end
 
